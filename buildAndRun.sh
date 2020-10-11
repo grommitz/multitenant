8d3d4bccac840ab920a6d7f7042b2e6ef3dd1369
grommitz <martincharlesworth@gmail.com>
2020-10-11 23:40:31 +0100

    Improves run script

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+VER=5.2020.4
+
+if [[ ! -f payara-micro.jar ]]; then
+  echo "Downloading payara micro..."
+  wget -O payara-micro.jar https://search.maven.org/remotecontent?filepath=fish/payara/extras/payara-micro/$VER/payara-micro-$VER.jar
+fi
+
 mvn clean package
 
-java -jar payara-micro-5.184.jar \
+java -jar payara-micro.jar \
     --nocluster \
     --deploy target/multitenant-1.0-SNAPSHOT.war
